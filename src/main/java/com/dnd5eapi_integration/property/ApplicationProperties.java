c8c9d6ea6bde07446a64c852209ae767966cbc0b
Jordan <jordloop@gmail.com>
2024-09-14 11:57:13 -0500

    replace application.properties with application.yaml. add ApplicationProperties

---
+++ src/main/java/com/dnd5eapi_integration/property/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.dnd5eapi_integration.property;
+
+import lombok.Getter;
+//import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class ApplicationProperties {
+    @Value("${data.service.dnd5eApiUrl}")
+    private String Dnd5eApiUrl;
+
+}
